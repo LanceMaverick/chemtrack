c5d4eea7bc3ef8c5cc6db9398dc91e34daa82bcf
LanceMaverick <pnoel6@gmail.com>
2016-09-16 23:38:20 +0200

    test results page registered to urls

--- chemserver/reagents/urls.py
+++ chemserver/reagents/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.index, name='index'),
+        url(r'^$', views.reagent_search, name='index'),
+        url(r'^results/(?P<results>[\w\-]+)/$', views.reagent_results, name='results')
         ]
