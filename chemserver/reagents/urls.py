a2b4972277d5ed9382da3c8c0b9a7067b71a8aa1
LanceMaverick <pnoel6@gmail.com>
2016-09-23 15:52:48 +0200

    url structure modified

--- chemserver/reagents/urls.py
+++ chemserver/reagents/urls.py
@@ -1,12 +1,43 @@
 from django.conf.urls import url
 from . import views
+from .models import Reagent, LiquidEntry, SolidEntry
+from .forms import AddReagentForm, LiquidEntryForm, SolidEntryForm
 
 urlpatterns = [
         url(r'^$', views.index, name='index'),
-        url(r'^search/$', views.reagent_search, name='reagent_search'),
-        url(r'^add_reagent/$', views.add_reagent, name='add_reagent'),
-        url(r'^results/(?P<results>[\w\-]+)/$', views.reagent_results, name='results'),
-        url(r'^add_solid/$', views.add_solid_entry, name = 'add_solid'),
-        url(r'^add_liquid/$', views.add_liquid_entry, name = 'add_liquid'),
-        url(r'^summary/$', views.show_entries, name = 'summary'),
+        
+        url(r'^add/solid/$', views.AddEntryView.as_view(
+            model=SolidEntry, form_class = SolidEntryForm), 
+            name = 'add_solid'),
+        
+        url(r'^add/liquid/$', views.AddEntryView.as_view(
+            model=LiquidEntry, form_class = LiquidEntryForm), 
+            name = 'add_liquid'),
+        
+        url(r'^add/reagent/$', views.AddEntryView.as_view(
+            model=Reagent, form_class = AddReagentForm),
+            name = 'add_reagent'),
+        
+        #TODO: implement model choosing logic in CBV for something like this:
+        #url(r'^add/(?P<mtype>.*)/$', views.AddEntryView.as_view(), name = 'add_entry'),
+        
+        url(r'^summary/solid/$', views.show_entries_solid, name = 'summary_solid'),
+        
+        url(r'^summary/liquid/$', views.show_entries_liquid, name = 'summary_liquid'),
+        
+        url(r'^month_summary/solid/(?P<year>[0-9]{4})/(?P<month>[0-9]+)/$',
+            views.MonthlySummarySolid.as_view(
+                month_format = '%m', template_name = 'reagents/entry_table_solids.html'), 
+                name = 'month_summary_solid'),
+        
+        url(r'^month_summary/liquid/(?P<year>[0-9]{4})/(?P<month>[0-9]+)/$',
+            views.MonthlySummaryLiquid.as_view(
+                month_format = '%m', template_name = 'reagents/entry_table_liquids.html'), 
+            name = 'month_summary_liquid'),
+        
+        url(r'^register/$', views.UserAuthView.as_view(), name = 'register'),
+        
         ]
+
+
+
